e423bfebcfb36b96158caf89f67a804f2f3b7a52
Rumi Maharjan <rumimaharjan99@gmail.com>
2024-06-17 14:28:28 +0545

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import Test from "@/components/homepage";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import HomePage from "@/components/homepage";
 
 const Home: React.FC = () => {
+  const [contentLoaded, setContentLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setContentLoaded(true);
+    }, 2000);
+  }, []);
+
+
     return (
         <>
-            <Test/>
+            {!contentLoaded ? (
+              <div className="background-gradient flex items-center justify-center ">
+                <img src="/prohdeen.jpg"/>
+              </div>
+            ) : (
+              <HomePage />
+            )}
         </>
     );
 };
