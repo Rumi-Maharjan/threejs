e96d7adffc3e8ade8e12d70c687a6fe6806afd06
Rumi Maharjan <rumimaharjan99@gmail.com>
2024-06-17 15:08:57 +0545

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,23 @@ import HomePage from "@/components/homepage";
 const Home: React.FC = () => {
   const [contentLoaded, setContentLoaded] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setContentLoaded(true);
-    }, 2000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setContentLoaded(true);
+  //   }, 2000);
+  // }, []);
 
 
     return (
         <>
-            {!contentLoaded ? (
+            {/* {!contentLoaded ? (
               <div className="background-gradient flex items-center justify-center ">
                 <img src="/prohdeen.jpg"/>
               </div>
             ) : (
               <HomePage />
-            )}
+            )} */}
+            <HomePage />
         </>
     );
 };
